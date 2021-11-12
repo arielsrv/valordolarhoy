22b294432fbe2f6e2dde5d071ac6cbe08b241794
Ariel Pineiro <arielsrv@gmail.com>
2021-11-12 08:43:42 -0300

    [test]

--- ValorDolarHoy/Startup.cs
+++ ValorDolarHoy/Startup.cs
@@ -72,9 +72,7 @@ namespace ValorDolarHoy
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ValorDolarHoy v1"));
+                app.UseDeveloperExceptionPage();                
             }
             else
             {
@@ -82,6 +80,9 @@ namespace ValorDolarHoy
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ValorDolarHoy v1"));
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
