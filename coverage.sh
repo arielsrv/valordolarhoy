625681158e7cf77609555a89d705809d4513c6b0
Ariel Piñeiro <arielsrv@gmail.com>
2021-12-04 08:31:36 -0300

    test

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,2 @@
 dotnet test /p:CollectCoverage=true /p:CoverletOutput="../CodeCoverage/" /p:CoverletOutputFormat="opencover"
-reportgenerator "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" 
+reportgenerator "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" "-assemblyfilters:-ValorDolarHoy;-ValorDolarHoy.Views" 
