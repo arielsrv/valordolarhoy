4ca738e4d361680e7644abafc25cb9e709ab705b
Ariel Piñeiro <arielsrv@gmail.com>
2021-12-04 08:06:59 -0300

    coverage

---
+++ coverage.sh
@@ -0,0 +1,2 @@
+dotnet test /p:CollectCoverage=true /p:CoverletOutput="../CodeCoverage/" /p:CoverletOutputFormat="opencover"
+reportgenerator "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" 
