46371a790285fb68bcd7cd7cf9fcf273d8094c3e
Ariel Piñeiro <arielsrv@gmail.com>
2021-12-07 09:06:01 -0300

    fix

--- ValorDolarHoy/Extensions/IServiceCollectionsExtensions.cs
+++ ValorDolarHoy/Extensions/IServiceCollectionsExtensions.cs
@@ -10,14 +10,12 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class IServiceCollectionsExtensions
     {
-        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<CurrencyService>();
             services.AddSingleton<IKeyValueStore, RedisStore>();
             services.AddSingleton<IRedisClientsManagerAsync, PooledRedisClientManager>(_ =>
                 new PooledRedisClientManager(configuration["Storage:Redis"]));
-
-            return services;
         }
 
         public static IServiceCollection AddClients(this IServiceCollection services)
