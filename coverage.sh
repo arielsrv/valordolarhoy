e2228410693007dd2448cb3f6dc35f2322b20334
Ariel Piñeiro <arielsrv@gmail.com>
2021-12-11 15:22:41 -0300

    nuget tool

--- coverage.sh
+++ coverage.sh
@@ -1,2 +1,2 @@
 dotnet test /p:CollectCoverage=true /p:CoverletOutput="../CodeCoverage/" /p:CoverletOutputFormat="opencover"
-reportgenerator "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" "-assemblyfilters:-ValorDolarHoy;-ValorDolarHoy.Views;" "-classfilters:-*Exception"
+dotnet /Users/"$USER"/.nuget/packages/reportgenerator/5.0.0/tools/net6.0/ReportGenerator.dll "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" "-assemblyfilters:-ValorDolarHoy;-ValorDolarHoy.Views;" "-classfilters:-*Exception"
