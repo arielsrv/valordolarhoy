a83727381da60b107f9a7fb0d34b8f21cd706db4
Ariel Pineiro <arielsrv@gmail.com>
2021-12-12 09:45:25 -0300

    Web

---
+++ build.cmd
@@ -0,0 +1,6 @@
+@echo off
+cmd /c "cd /d ValorDolarHoy\ClientApp && npm cache verify"	
+dotnet restore
+dotnet build
+dotnet test /p:CollectCoverage=true /p:CoverletOutput="..\CodeCoverage" /p:CoverletOutputFormat="opencover" --logger:"console;verbosity=detailed"
+dotnet %USERPROFILE%\.nuget\packages\reportgenerator\5.0.0\tools\net6.0\ReportGenerator.dll "-reports:CodeCoverage\coverage.opencover.xml" "-targetdir:CodeCoverage\Web" "-assemblyfilters:-ValorDolarHoy;-ValorDolarHoy.Views;" "-classfilters:-*Exception"
