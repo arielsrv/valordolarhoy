8654aadfe0c5369d7f4b658f63113799f6777f6f
Ariel Piñeiro <arielsrv@gmail.com>
2021-12-13 05:53:50 -0300

    fix

--- ValorDolarHoy/Pages/Error.cshtml.cs
+++ ValorDolarHoy/Pages/Error.cshtml.cs
@@ -3,21 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace ValorDolarHoy.Pages;
-
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-public class ErrorModel : PageModel
+namespace ValorDolarHoy.Pages
 {
-    public ErrorModel(ILogger<ErrorModel> _logger)
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorModel : PageModel
     {
-    }
+        public ErrorModel(ILogger<ErrorModel> _logger)
+        {
+        }
 
-    public string RequestId { get; private set; }
+        public string RequestId { get; private set; }
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
 
-    public void OnGet()
-    {
-        this.RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier;
+        public void OnGet()
+        {
+            this.RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier;
+        }
     }
 }
