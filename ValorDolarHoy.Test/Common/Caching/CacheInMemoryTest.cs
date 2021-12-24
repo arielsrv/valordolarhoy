d09f8ed6cac10ad0b61032cdaa2a385e405cb27b
Ariel Piñeiro <arielsrv@gmail.com>
2021-12-24 16:41:34 -0300

    fix

--- ValorDolarHoy.Test/Common/Caching/CacheInMemoryTest.cs
+++ ValorDolarHoy.Test/Common/Caching/CacheInMemoryTest.cs
@@ -1,6 +1,58 @@
+using System;
+using System.Threading;
+using ValorDolarHoy.Core.Common.Caching;
+using Xunit;
+
 namespace ValorDolarHoy.Test.Common.Caching;
 
 public class CacheInMemoryTest
 {
-    
+    [Fact]
+    public void Hit()
+    {
+        ICache<string, string> appCache = CacheBuilder<string, string>
+            .NewBuilder()
+            .Size(2)
+            .ExpireAfterWrite(TimeSpan.FromMinutes(int.MaxValue))
+            .Build();
+
+        appCache.Put("key", "value");
+
+        string? actual = appCache.GetIfPresent("key");
+
+        Assert.NotNull(actual);
+        Assert.Equal("value", actual);
+    }
+
+    [Fact]
+    public void Miss()
+    {
+        ICache<string, string> appCache = CacheBuilder<string, string>
+            .NewBuilder()
+            .Size(2)
+            .ExpireAfterWrite(TimeSpan.FromMinutes(int.MaxValue))
+            .Build();
+
+        string? actual = appCache.GetIfPresent("key");
+
+        Assert.Null(actual);
+    }
+
+    [Fact]
+    public void Expire_After_Write()
+    {
+        ICache<string, string> appCache = CacheBuilder<string, string>
+            .NewBuilder()
+            .Size(2)
+            .ExpireAfterWrite(TimeSpan.FromMilliseconds(100))
+            .Build();
+
+        appCache.Put("key", "value");
+
+        Thread.Sleep(200);
+
+        string? actual = appCache.GetIfPresent("key");
+
+        Assert.Null(actual);
+    }
 }
