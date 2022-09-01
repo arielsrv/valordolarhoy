6d87f49cbb84060a52f39041e19e3350fc51a517
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-09-01 10:37:05 +0200

    xml

--- ValorDolarHoy/Pages/Error.cshtml.cs
+++ ValorDolarHoy/Pages/Error.cshtml.cs
@@ -4,15 +4,28 @@ using Microsoft.Extensions.Logging;
 
 namespace ValorDolarHoy.Pages;
 
+/// <summary>
+/// Error model
+/// </summary>
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 public class ErrorModel : PageModel
 {
+    /// <summary>
+    /// ErrorModel
+    /// </summary>
+    /// <param name="logger"></param>
     public ErrorModel(ILogger<ErrorModel> logger)
     {
         this.RequestId = string.Empty;
     }
 
+    /// <summary>
+    /// RequestId
+    /// </summary>
     public string RequestId { get; }
 
+    /// <summary>
+    /// ShowRequestId
+    /// </summary>
     public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
 }
--- ValorDolarHoy/Program.cs
+++ ValorDolarHoy/Program.cs
@@ -1,7 +1,14 @@
 namespace ValorDolarHoy;
 
+/// <summary>
+/// Main
+/// </summary>
 public static class Program
 {
+    /// <summary>
+    /// Run
+    /// </summary>
+    /// <param name="args"></param>
     public static void Main(string[] args)
     {
         WebServer.Run(args);
