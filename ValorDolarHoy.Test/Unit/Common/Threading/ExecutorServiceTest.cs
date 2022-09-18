ca9f806a2958ec98eaefcc3a9b5a6cb2954666e9
ariel.pineiro <ariel.pineiro@iskaypet.com>
2022-09-18 15:19:52 +0200

    fix

--- ValorDolarHoy.Test/Unit/Common/Threading/ExecutorServiceTest.cs
+++ ValorDolarHoy.Test/Unit/Common/Threading/ExecutorServiceTest.cs
@@ -10,12 +10,12 @@ public class ExecutorServiceTest
     [Fact]
     public void Fire_And_Forget()
     {
-        ExecutorService executorService = Executors.NewFixedThreadPool(10);
+        ExecutorService executorService = Executors.NewFixedThreadPool(Environment.ProcessorCount - 1);
 
         int value = 0;
         executorService.Run(() =>
         {
-            Thread.Sleep(TimeSpan.FromMilliseconds(500));
+            Thread.Sleep(TimeSpan.FromMilliseconds(1000));
             value = int.MaxValue;
         });
 
@@ -25,12 +25,12 @@ public class ExecutorServiceTest
     [Fact]
     public void Fire_And_Forget_Expected_Value()
     {
-        ExecutorService executorService = Executors.NewFixedThreadPool(10);
+        ExecutorService executorService = Executors.NewFixedThreadPool(Environment.ProcessorCount - 1);
 
         int value = 0;
         executorService.Run(() => { value = int.MaxValue; });
 
-        Thread.Sleep(TimeSpan.FromMilliseconds(500));
+        Thread.Sleep(TimeSpan.FromMilliseconds(1000));
         Assert.Equal(int.MaxValue, value);
     }
 }
