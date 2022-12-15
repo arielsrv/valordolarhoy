941275bf1412465fa6ff68db9153955b56d31fff
ariel.pineiro@iskaypet.com <arielsrv@gmail.com>
2022-12-15 10:28:33 -0300

    updatefix

--- coverage.sh
+++ coverage.sh
@@ -3,10 +3,10 @@ echo ">>> Running test..."
 echo
 
 export CollectCoverage=true
-export CoverletOutput=../CodeCoverage/
-export CoverletOutputFormat=opencover
+#export CoverletOutput=../CodeCoverage/
+#export CoverletOutputFormat=opencover
 
-dotnet test ValorDolarHoy.sln --no-build $CollectCoverage $CoverletOutput $CoverletOutputFormat
+dotnet test ValorDolarHoy.sln --no-build $CollectCoverage
 echo ">>> Build coverage report..."
 echo
 dotnet /Users/"$USER"/.nuget/packages/reportgenerator/5.1.12/tools/net7.0/ReportGenerator.dll "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" "-assemblyfilters:-Web;-Web.Views;" "-classfilters:-*Exception"
