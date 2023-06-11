069b9c69286727bb6e705c8ac3401dc772f8220c
arielpineiro <arielsrv@gmail.com>
2023-06-11 11:19:10 +0200

    upgrade

--- coverage.sh
+++ coverage.sh
@@ -10,7 +10,7 @@ export CoverletOutputFormat=opencover
 dotnet test $SolutionFile --no-build $CollectCoverage
 echo ">>> Build coverage report..."
 echo
-dotnet /Users/"$USER"/.nuget/packages/reportgenerator/5.1.15/tools/net7.0/ReportGenerator.dll "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" "-assemblyfilters:-Web;-Web.Views;" "-classfilters:-*Exception"
+dotnet /Users/"$USER"/.nuget/packages/reportgenerator/5.1.22/tools/net7.0/ReportGenerator.dll "-reports:CodeCoverage/coverage.opencover.xml" "-targetdir:CodeCoverage/Web" "-assemblyfilters:-Web;-Web.Views;" "-classfilters:-*Exception"
 
 echo
 open CodeCoverage/Web/index.html
